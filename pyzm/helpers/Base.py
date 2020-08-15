9d5cdca6419c0e6c0ad646cd06bd1b5ff45a52d4
Pliable Pixels <pliablepixels@gmail.com>
2020-08-15 07:48:54 -0400

    make logs consistent, add ms

--- pyzm/helpers/Base.py
+++ pyzm/helpers/Base.py
@@ -23,31 +23,31 @@ class Base:
 class SimpleLog:
     ' console based logging function that is used if no logging handler is passed'
     def __init__(self):
-        pass
+        self.dtformat = "%b %d %Y %H:%M:%S.%f"
 
     def Debug (self,level, message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S.%f")
+        dt = datetime.now().strftime(self.dtformat)
 
-        print ('{} [DEBUG {}] {}'.format(dt, level, message))
+        print ('{} [DBG {}] {}'.format(dt, level, message))
 
     def Info (self,message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S")
-        print ('{} [INFO] {}'.format( dt, message))
+        dt = datetime.now().strftime(self.dtformat)
+        print ('{} [INF] {}'.format( dt, message))
 
     def Warning (self,message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S")
-        print ('{}  [WARNING] {}'.format( dt, message))
+        dt = datetime.now().strftime(self.dtformat)
+        print ('{}  [WAR] {}'.format( dt, message))
 
     def Error (self,message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S")
-        print ('{} [ERROR] {}'.format(dt, message))
+        dt = datetime.now().strftime(self.dtformat)
+        print ('{} [ERR] {}'.format(dt, message))
 
     def Fatal (self,message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S")
-        print ('{} [FATAL] {}'.format(dt, message))
+        dt = datetime.now().strftime(self.dtformat)
+        print ('{} [FAT] {}'.format(dt, message))
         exit(-1)
 
     def Panic (self,message, caller=None):
-        dt = datetime.now().strftime("%b %d %Y %H:%M:%S")
-        print ('{} [PANIC] {}'.format(dt, message))
+        dt = datetime.now().strftime(self.dtformat)
+        print ('{} [PNC] {}'.format(dt, message))
         exit(-2)
