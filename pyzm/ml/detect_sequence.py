8918c63fbccf31a988d957d107ddc558e51cdce7
Pliable Pixels <pliablepixels@gmail.com>
2021-04-04 10:17:23 -0400

    delay_between_snapshots

--- pyzm/ml/detect_sequence.py
+++ pyzm/ml/detect_sequence.py
@@ -530,7 +530,8 @@ class DetectSequence(Base):
             options (dict, optional): Various options that control the detection process. Defaults to {}:
             
                 - delay (int): Delay in seconds before starting media stream
-                - delay_between_frames (int): Delay in seconds between each frame read (useful if you want to read snapshot multiple times, for example)
+                - delay_between_frames (int): Delay in seconds between each frame read 
+                - delay_between_snapshots (int): Delay in seconds between each snapshot frame read (useful if you want to read snapshot multiple times, for example. frameset: ['snapshot','snapshot','snapshot'])
                 - download (boolean): if True, will download video before analysis. Defaults to False
                 - download_dir (string): directory where downloads will be kept (only applies to videos). Default is /tmp
                 - start_frame (int): Which frame to start analysis. Default 1.
