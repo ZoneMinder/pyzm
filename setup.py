9938ea7b7a92a9dee11f8c43c516b22e28aed1e3
Michael Ludvig <mludvig@logix.net.nz>
2021-08-15 12:27:36 +1200

    Add aws_rekognition to the modules list

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(name = NAME,
                     'pyzm.ml.face_train_dlib',
                     'pyzm.ml.object',
                     'pyzm.ml.coral_edgetpu',
+                    'pyzm.ml.aws_rekognition',
                     'pyzm.ml.hog',
                     'pyzm.ml.yolo',
                     'pyzm.ml.detect_sequence'
