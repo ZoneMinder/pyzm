24547c3d75139d01c4b0ced933ab47b25665268d
baudneo <baudneo@protonmail.com>
2021-12-10 02:46:25 -0700

    WOW

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ setup(name=NAME,
           'pyzm.ml.coral_edgetpu',
           'pyzm.ml.hog',
           'pyzm.ml.yolo',
+          'pyzm.ml.aws_rekognition',
           'pyzm.ml.detect_sequence',
       ]
       )
